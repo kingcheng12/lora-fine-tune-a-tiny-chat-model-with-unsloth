3ee57adfcf00c38ff7c0b012df8e43b2257b6536
kingcheng12 <gc169@duke.edu>
2026-07-09 19:36:47 -0400

    Step 13: tokenize_text

--- model.py
+++ model.py
@@ -139,8 +139,15 @@ def build_text_dataset(texts):
     
     return Dataset.from_dict({"text": texts})
 
-# Step 13 - tokenize_text (not yet solved)
-# TODO: implement
+# Step 13 - tokenize_text
+def tokenize_text(tokenizer, text):
+    """Tokenize a single string and return a list[int] of input ids."""
+    # TODO: call the tokenizer on text and return its input_ids as a plain list
+    
+    encoded = tokenizer(text)
+    input_ids = list(encoded["input_ids"])
+
+    return input_ids
 
 # Step 14 - count_tokens (not yet solved)
 # TODO: implement
